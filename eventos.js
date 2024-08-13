53218a3eecc081dd60b0a02732d5ccd253e4a9e6
sofia-valdez <valdezsofia7@gmail.com>
2024-08-12 22:54:01 -0300

    solution click

--- eventos.js
+++ eventos.js
@@ -2,11 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
 let buttonDiv = document.querySelector('.div-button')
 console.log('buttonDiv', buttonDiv)
-buttonDiv.addEventListener("click", function(){
+buttonDiv.addEventListener("click", function(event){
 alert("Hola, soy el div")
 });
 })
 
 function myFunction() {
     alert("Hola!")
+    event.stopPropagation();
   }
